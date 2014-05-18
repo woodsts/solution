059f279b47012e040de6deedd2ecac82755cbe83
Derald D. Woods <woods.technical@gmail.com>
2014-05-18 17:45:16 -0500

    solution.mk: add common variables for toolchain binaries

--- solution.mk
+++ solution.mk
@@ -6,9 +6,24 @@
 # This file is part of the solution project, and is made available
 # under the terms of the GNU General Public License version 2
 #
+# http://www.arm.linux.org.uk/docs/kerncomp.php
+#
 
 ELDS := $(shell readlink -e $(CURDIR))
 
+ELDS_ARCH ?= arm
+ELDS_VENDOR ?= -unknown
+ELDS_OS ?= linux
+ELDS_ABI ?= gnueabi
+ELDS_CROSS_TUPLE := $(ELDS_ARCH)$(ELDS_VENDOR)-$(ELDS_OS)-$(ELDS_ABI)
+ELDS_CROSS_COMPILE := $(ELDS_CROSS_TUPLE)-
+ELDS_CROSS_PARAMS := ARCH=$(ELDS_ARCH) CROSS_COMPILE=$(ELDS_CROSS_COMPILE)
+
+TOOLCHAIN := $(ELDS)/toolchain/$(ELDS_CROSS_TUPLE)
+TOOLCHAIN_BUILD := $(ELDS)/toolchain/build/$(ELDS_CROSS_TUPLE)
+TOOLCHAIN_CONFIG := $(TOOLCHAIN_BUILD)/.config
+
 CMD := $(shell echo $(ELDS) > $(ELDS)/.solution)
+CMD := $(shell echo $(ELDS_CROSS_TUPLE) > $(ELDS)/.cross-tuple)
 
 export ELDS
