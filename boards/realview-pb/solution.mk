5fd58ad1e57c57ede5b44cdfc39f8711e8f4b71e
Derald D. Woods <woods.technical@gmail.com>
2014-06-06 21:44:38 -0500

    realview-pb: Add ARM Cortex-A8 board for QEMU

---
+++ boards/realview-pb/solution.mk
@@ -0,0 +1,33 @@
+#
+# This is the GNU Make include file for 'solution'
+#
+# Copyright (C) 2014 Derald D. Woods
+#
+# This file is part of the solution project, and is made available
+# under the terms of the GNU General Public License version 2
+#
+
+BOARD_HOSTNAME := realview-pb
+BOARD_GETTY_PORT := ttyAMA0
+
+BOARD_ARCH ?= arm
+BOARD_VENDOR ?= -unknown
+BOARD_OS ?= linux
+BOARD_ABI ?= gnueabihf
+
+BOARD_TOOLCHAIN_CONFIG := $(ELDS)/boards/$(ELDS_BOARD)/config/crosstool-ng/config
+BOARD_ROOTFS_CONFIG := $(ELDS)/boards/$(ELDS_BOARD)/config/buildroot/config
+BOARD_KERNEL_CONFIG := $(ELDS)/boards/$(ELDS_BOARD)/config/linux/config
+
+define realview-pb-env
+	@printf "BOARD_ARCH                  : $(BOARD_ARCH)\n"
+	@printf "BOARD_VENDOR                : $(BOARD_VENDOR)\n"
+	@printf "BOARD_OS                    : $(BOARD_OS)\n"
+	@printf "BOARD_ABI                   : $(BOARD_ABI)\n"
+	@printf "BOARD_HOSTNAME              : $(BOARD_HOSTNAME)\n"
+	@printf "BOARD_GETTY_PORT            : $(BOARD_GETTY_PORT)\n"
+endef
+
+export BOARD_HOSTNAME
+export BOARD_GETTY_PORT
+
